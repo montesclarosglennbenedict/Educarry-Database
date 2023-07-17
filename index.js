e7dc9c7a72c4a298f809711fa20275677b0f3e82
montesclarosglennbenedict <84988544+montesclarosglennbenedict@users.noreply.github.com>
2023-07-17 18:56:41 +0000

    The database works, I guess. It's missing the reports but I couldn't be bothered to deal with PDFs, so I'll wait for the API for articulations

--- index.js
+++ index.js
@@ -3,38 +3,70 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
+const cors = require('cors');
 
-/*
-app.get('/api/data', (req, res) => {
-  const articulation = req.query.articulation; // Access the value of the 'articulation' query parameter
+app.get('/api/transferability/categories', (req, res) => {
+  const institutionId = req.query.institutionId; // Access the value of the 'articulation' query parameter
+  const academicYearId = req.query.academicYearId;
+  const listType = req.query.listType;
 
   // Use the articulation value in your logic
   // For example, you can modify the file path based on the query parameter
-  const dataPath = path.join(__dirname, 'api', `data_${articulation}.json`);
+  const dataPath = path.join(__dirname, 'api', 'transferability', 'categories', `${institutionId}-${academicYearId}-${listType}.json`);
 
   fs.readFile(dataPath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-    const jsonData = JSON.parse(data);
-    res.json(jsonData);
+    res.send(data);
   });
-});*/
+});
+
+app.get('/api/transferability/courses', (req, res) => {
+  const institutionId = req.query.institutionId; // Access the value of the 'articulation' query parameter
+  const academicYearId = req.query.academicYearId;
+  const listType = req.query.listType;
+
+  // Use the articulation value in your logic
+  // For example, you can modify the file path based on the query parameter
+  const dataPath = path.join(__dirname, 'api', 'transferability', 'categories', `${institutionId}-${academicYearId}-${listType}.json`);
+
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.send(data);
+  });
+});
+
 
 
 app.get('/api/AcademicYears', (req, res) => {
   // Use the articulation value in your logic
   // For example, you can modify the file path based on the query parameter
   const dataPath = path.join(__dirname, '/api/AcademicYears', `index.json`);
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.send(data);
+  });
+});
+
+app.get('/api/institutions', (req, res) => {
+  // Use the articulation value in your logic
+  // For example, you can modify the file path based on the query parameter
+  const dataPath = path.join(__dirname, '/api/institutions', `index.json`);
 
   fs.readFile(dataPath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-    const jsonData = JSON.parse(data);
-    res.json(jsonData);
+    res.send(data);
   });
 });
 
@@ -48,8 +80,7 @@ app.get('/api/appsettings', (req, res) => {
       console.error(err);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-    const jsonData = JSON.parse(data);
-    res.json(jsonData);
+    res.send(data);
   });
 });
 
@@ -62,12 +93,20 @@ app.get('/api/institutions/*/transferability/availableAcademicYears', (req, res)
       console.error(err);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
-    const jsonData = JSON.parse(data);
-    res.json(jsonData);
+    res.send(data);
   });
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
+app.use(cors({
+  origin: 'https://educarry2.montesclarosglennbenedict.repl.co',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
 const port = 3000;
 app.listen(port, () => {
